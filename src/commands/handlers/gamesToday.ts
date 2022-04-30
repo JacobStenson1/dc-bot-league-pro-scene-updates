6cd480985c79ab87ae958272e4b3e8fb17419302
Jacob Stenson <jacob@nosnets.co.uk>
2022-04-30 19:52:13 +0100

    Returns the games today in a readable format for a specified league ID

--- src/commands/handlers/gamesToday.ts
+++ src/commands/handlers/gamesToday.ts
@@ -1,7 +1,9 @@
 import editInteractionReply from '../../functions/editInteractionReply';
 import { getAllGamesForLeague } from '../../api/games';
 import { CommandInteraction } from 'discord.js';
-import getItemsToday from '../../utils/getItemsToday';
+import getItemsToday from '../../utils/functions/getItemsToday';
+import { Game } from '../../utils/types/interfaces/scheduleGame';
+import moment from 'moment';
 
 export default async (interaction: CommandInteraction) => {
 
@@ -11,7 +13,24 @@ export default async (interaction: CommandInteraction) => {
 
     const leagueGames = await getAllGamesForLeague(leagueId);
 
-    const gamesToday = getItemsToday(leagueGames.data.schedule.events, 'startTime')
+    const gamesToday = getItemsToday(leagueGames.data.schedule.events, 'startTime') as Game[];
 
-    return editInteractionReply(interaction, JSON.stringify(gamesToday));
+    console.log("gamesToday");
+    console.log(gamesToday);
+
+    let gamesTodayString = `League pro games today for League ID: ${leagueId}:\n\n`;
+    for (const game of gamesToday) {
+        console.log(game);
+        gamesTodayString +=
+            `Start time: ${moment(game.startTime).format('DD/MM/YYYY HH:mm')}\n` +
+            `League: ${game.league.name}\n`+
+            `Teams: ${game?.match?.teams?.map((team) => {
+                console.log("team");
+                console.log(team);
+                return team.name + ' & '
+            })}\n`+
+            `\n`
+    }
+
+    return editInteractionReply(interaction, gamesTodayString);
 }
